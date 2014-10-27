8c131dd29bae6e6ada65c4d24213f22d22f87dca
Jeremy Leipzig <leipzigj@email.chop.edu>
2014-10-27 16:22:06 -0400

    urls cleanup

--- mybic/conf/urls.py
+++ mybic/conf/urls.py
@@ -8,30 +8,24 @@ from django.conf import settings
 
 admin.autodiscover()
 
-#def my_view(request):
-#    return render(request, get_ab_template(request, 'page.html'))
-
-#http://stackoverflow.com/questions/15059519/what-is-equivalent-of-direct-to-template-in-class-based-view-in-django
 urlpatterns = patterns(
     '',
     url(r'^$',include('chopauth.urls')),
     url(r'', include('chopauth.urls')),
-    url(r'^dashboard/{0,1}$', 'mybic.views.dashboard', name='dashboard'),
+    
+    url(r'^admin/', include(admin.site.urls)),
+    
     url(r'^accounts/login/{0,1}$', 'mybic.views.dashboard', name='dashboard'),
+    url(r'^dashboard/{0,1}$', 'mybic.views.dashboard', name='dashboard'),
     url(r'^/{0,1}$', 'mybic.views.dashboard', name='dashboard'),
 
-    #url(r'^page/$', my_view, name='my-page'),
     # Administrative components
-    url(r'^admin/', include(admin.site.urls)),
+    
     url(r'^labs/$','mybic.views.dashboard', name='dashboard'),
-    url(r'^labs/(\w+)/$', 'mybic.labs.views.labview', name='my_lab_url'),
-    url(r'^labs/(\w+)/(\w+)/$', 'mybic.labs.views.projectview', name='my_project_url'),
+    url(r'^labs/([\w-]+)/$', 'mybic.labs.views.labview', name='my_lab_url'),
+    url(r'^labs/([\w-]+)/([\w-]+)/$', 'mybic.labs.views.projectview', name='my_project_url'),
 
     url(r'^slink/(?P<path>.*)$','mybic.views.protected_file',name='protected')
     
-    #url(r'^labs/', include('mybic.labs.urls')),
-    #url(r'^labs/(\S+)$', 'mybic.views.labs'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-#url(r'^$', TemplateView.as_view(template_name='index.html')),
-#url(r'^accounts/login/{0,1}$', TemplateView.as_view(template_name='foo_index.html'))
