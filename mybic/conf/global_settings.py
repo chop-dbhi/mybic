9ea6f78f1dea129fe1b75333506218243259ecdc
Jeremy Leipzig <leipzigj@email.chop.edu>
2014-10-28 16:26:23 -0400

    removed simple templates. added dev tag

--- mybic/conf/global_settings.py
+++ mybic/conf/global_settings.py
@@ -108,7 +108,6 @@ PROTECTED_URL = '/slink/'
 # os.path.join(BASE_PATH,'_site/static/pei_lab'),
 # os.path.join(BASE_PATH,'_site/static/pei_lab/errRNAseq'),
 STATICFILES_DIRS = (
-    os.path.join(BASE_PATH,'mybic/labs/templates/'),
 )
 
 # Templates
@@ -116,6 +115,7 @@ STATICFILES_DIRS = (
 TEMPLATE_CONTEXT_PROCESSORS += (
     'django.core.context_processors.request',
     'mybic.context_processors.static',
+    'mybic.context_processors.development',
 )
 
 # List of callables that know how to import templates from various sources.
@@ -134,8 +134,6 @@ TEMPLATE_LOADERS = (
 #)
 
 
-
-SIMPLE_TEMPLATES_DIR = 'simple_templates'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -193,7 +191,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'simple_templates.middleware.SimplePageFallbackMiddleware',
     'siteauth.middleware.SiteAuthenticationMiddleware'
 )
 
