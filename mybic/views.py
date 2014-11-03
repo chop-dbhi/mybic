ffcd5f8a3c8259721056cad3ac0348f6e1f7da03
Jeremy Leipzig <leipzigj@email.chop.edu>
2014-11-03 15:20:33 -0500

    added more content types

--- mybic/views.py
+++ mybic/views.py
@@ -48,14 +48,33 @@ def protected_file(request,lab,project,path):
         response['X-Accel-Redirect'] = 'test'
     else:
         if hasattr(request, 'user') and request.user.is_authenticated():
+            kwargs = {'user': request.user}
+            user = request.user
+            if user.is_staff:
+                my_groups = Group.objects.all()
+                my_groups_list = my_groups.values_list('name', flat=True)
+            else:
+                my_groups = Group.objects.filter(user=request.user)
+                my_groups_list = my_groups.values_list('name',flat=True)
+
             try:
                 lab_object = Lab.objects.get(slug=lab)
                 project_object = Project.objects.get(slug=project)
             except ObjectDoesNotExist:
                 raise Http404
-            if lab_object.group in request.user.groups.all():
+            if lab_object.group in my_groups:
                 if path.endswith("pdf"):
                     response['Content-Type'] = 'application/pdf'
+                elif path.endswith("xlsx"):
+                    print >>sys.stderr,"xslx file!\n"
+                    response['Content-Type'] = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                    response['Content-Disposition'] = 'attachment; filename="{0}"'.format(os.path.basename(path))
+                elif path.endswith("xls"):
+                    print >>sys.stderr,"xls file!\n"
+                    response['Content-Type'] = 'application/vnd.ms-excel'
+                    response['Content-Disposition'] = 'attachment; filename="{0}"'.format(os.path.basename(path))
+                else:
+                    print >>sys.stderr,"some other file!\n"
                 response['X-Accel-Redirect'] = '{0}/{1}/{2}/{3}'.format('/protected/', lab, project, path)
             else:
                 return render_to_response('error.html',context_instance=RequestContext(request))
