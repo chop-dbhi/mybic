511a4c4a112dee4887e130dff56fe9b6077d35ab
Jeremy Leipzig <leipzigj@email.chop.edu>
2015-01-23 15:50:23 -0500

    toggle js not needed

--- mybic/static/scripts/javascript/src/mybic.js
+++ mybic/static/scripts/javascript/src/mybic.js
@@ -13,24 +13,5 @@
                 dataType: "json"
             });
         });
-        $('#toggle-masquerade').click(function(e) {
-            e.preventDefault();
-            var toggle_link = this;
-            $.ajax({
-                url: this.href,
-                type: "GET",
-                cache: false,
-                success: function(data) {
-                    console.log(data);
-                    if(data.state == true){
-                        $( toggle_link ).text("True")
-                    }else{
-                        $( toggle_link ).text("False")
-                    }
-                    location.reload();
-                },
-                dataType: "json"
-            });
-        });
     });
 })(jQuery);
